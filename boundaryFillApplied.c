30b21491ab3e02006267e2eae1fe7c4e4d404f3d
Shubham Arutwar <shubhamarutwar23@gmail.com>
2020-11-20 13:41:28 +0530

    made function for color fill

--- boundaryFillApplied.c
+++ boundaryFillApplied.c
@@ -17,6 +17,20 @@ void boundaryFill4(int x, int y, int fill_color,int boundary_color)
     }
 }
 
+void colorprint(int x1,int y1, int x2, int y2, int c)
+  {
+      int i , j;
+      int x , y;
+        for(i=x1;i<=x2;i=i+10)
+        {
+          for(j=y1; j<=y2; j=j+10)
+          {
+            x = i;
+            y = j;
+            boundaryFill4( x,  y, c , 15);
+          }
+        }
+  }
 
 void main()
 {
@@ -24,22 +38,23 @@ void main()
     int gd = DETECT, gm;
     int i , j;
     int x , y;
-
+    int c;
     initgraph(&gd, &gm, "C://TC3//BGI");
 
-    for(i=150; i<230; i = i+10 )
+    for(i=150; i<=300; i = i+10 )
     {
-    	for(j=230 ; j> 150 ; j= j-10)
+    	for(j=150; j<= 420 ; j= j+10)  //(300,420)
       {
       		x = i+5;
-      		y = j-5;
-      		rectangle(i , j , i+10 , j-10);
+      		y = j+5;
+      		rectangle(i , j , i+10 , j+10);
       		boundaryFill4( x,  y, 2  , 15);
   		}
   	}
+
 //eyes and mouth
         //left eye
-        for(i=165;i<176;i=i+10)
+        for(i=205;i<216;i=i+10)
         {
           for(j=175; j<186; j=j+10)
           {
@@ -50,7 +65,7 @@ void main()
         }
 
       //right eye
-      for(i=205;i<216;i=i+10)
+      for(i=245;i<256;i=i+10)
       {
         for(j=175; j<186; j=j+10)
         {
@@ -61,7 +76,7 @@ void main()
       }
 
       //mouth mid
-      for(i=185;i<196;i=i+10)
+      for(i=225;i<236;i=i+10)
       {
         for(j=195; j<216; j=j+10)
         {
@@ -70,36 +85,11 @@ void main()
           boundaryFill4( x,  y, 0 , 15);
         }
       }
-      //mouth left side
-      for(i=175;i<176;i=i+10)
-      {
-        for(j=205; j<226; j=j+10)
-        {
-          x = i;
-          y = j;
-          boundaryFill4( x,  y, 0 , 15);
-        }
-      }
-      //mouth right side
-      for(i=205;i<206;i=i+10)
-      {
-        for(j=205; j<226; j=j+10)
-        {
-          x = i;
-          y = j;
-          boundaryFill4( x,  y, 0 , 15);
-        }
-      }
+      colorprint(215,205,215,225,0);
+      colorprint(245,205,245,225,0);//mouth right side
 
 
     getch();
     closegraph();
-
-}
-void eyesandmouth(int i,int j,int x,int y)
-{
-
-      int gd = DETECT, gm;
-      initgraph(&gd, &gm, "C://TC3//BGI");
 
 }
